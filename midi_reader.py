50750d4c406502de94b5204989244833a80e6819
hauke <hauke.dau@googlemail.com>
2017-11-23 12:47:54 +0100

    keras working?

--- midi_reader.py
+++ midi_reader.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 import numpy as numpy
 
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.layers import Embedding
+from keras.layers import LSTM
+
+
 
 def loadMidiFromText():
     with open ('midiBach.txt', 'r') as myfile:
@@ -32,10 +38,26 @@ def createInputArray():
         except IndexError:
             print("Couldn't find informations"+str(splittedLine))
     return linesArray
-    
-print(createInputArray())
-        
-    
-    
-        
+inputArray = createInputArray()
+print(inputArray)
+
+model = Sequential()
+
+model.add(Embedding(88, output_dim=88))
+model.add(LSTM(128))
+model.add(Dropout(0.5))
+model.add(Dense(88, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+x_train = numpy.asarray(inputArray[:-1])
+y_train = numpy.asarray(inputArray[1:])
+
+model.fit(x_train, y_train, batch_size=1032, epochs=10)
+score = model.evaluate(x_train, y_train, batch_size=1032)
+result = model.predict(x_train, batch_size=1032)
+print(score)
+print(result.shape)
     
