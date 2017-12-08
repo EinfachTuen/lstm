d87a22f04e4df54aacca677dbfc8fcdc048c1bd5
Hauke Dau <4dau@informatik.uni-hamburg.de>
2017-12-08 16:34:20 +0100

    ooopdateddd

--- midiTraining.py
+++ midiTraining.py
@@ -10,7 +10,7 @@ from keras.utils import to_categorical
 from keras.optimizers import RMSprop
 from keras.layers import Activation
 
-import KTimage as kt
+import midiStringParts
 
 
 def loadMidiFromText():
@@ -45,123 +45,13 @@ def createInputArray():
     return linesArray
 
 
-
-
 def convertToMidi(x):
     result = numpy.asarray(x)
-    notenArray = numpy.zeros(2037, dtype=numpy.int)
     timeStep = 120
-    midiStartString = """MFile 1 17 480
-MTrk
-0 Meta TrkName "untitled"
-0 SMPTE 96 0 3 0 0
-0 TimeSig 4/4 24 8
-0 KeySig 0 major
-0 Tempo 600000
-0 Meta Marker "A"
-34560 Meta Marker "1."
-36480 Meta Marker "A'"
-71040 Meta Marker "2."
-72960 Meta Marker "B"
-123840 Tempo 1200000
-123840 Meta TrkEnd
-TrkEnd
-MTrk
-0 Meta 0x21 00
-0 Meta TrkName "Solo Flute"
-0 PrCh ch=1 p=73
-0 Par ch=1 c=7 v=100
-0 Par ch=1 c=10 v=64"""
-    Ende = """124800 Meta TrkEnd
-TrkEnd
-MTrk
-0 Meta 0x21 00
-0 Meta TrkName "--------------------------------------"
-0 Meta TrkEnd
-TrkEnd
-MTrk
-0 Meta 0x21 00
-0 Meta TrkName "Johann Sebastian Bach  (1685-1750)"
-0 Meta TrkEnd
-TrkEnd
-MTrk
-0 Meta 0x21 00
-0 Meta TrkName "--------------------------------------"
-0 Meta TrkEnd
-TrkEnd
-MTrk
-0 Meta 0x21 00
-0 Meta TrkName "Partita in A minor for Solo Flute - BWV 1013"
-0 Meta TrkEnd
-TrkEnd
-MTrk
-0 Meta 0x21 00
-0 Meta TrkName "--------------------------------------"
-0 Meta TrkEnd
-TrkEnd
-MTrk
-0 Meta 0x21 00
-0 Meta TrkName "1st Movement: Allemande"
-0 Meta TrkEnd
-TrkEnd
-MTrk
-0 Meta 0x21 00
-0 Meta TrkName "--------------------------------------"
-0 Meta TrkEnd
-TrkEnd
-MTrk
-0 Meta 0x21 00
-0 Meta TrkName "Sequenced with Cakewalk Pro Audio by"
-0 Meta TrkEnd
-TrkEnd
-MTrk
-0 Meta 0x21 00
-0 Meta TrkName "David J. Grossman - dave@unpronounceable.com"
-0 Meta TrkEnd
-TrkEnd
-MTrk
-0 Meta 0x21 00
-0 Meta TrkName "This and other Bach MIDI files can be found at:"
-0 Meta TrkEnd
-TrkEnd
-MTrk
-0 Meta 0x21 00
-0 Meta TrkName "Dave's J.S. Bach Page"
-0 Meta TrkEnd
-TrkEnd
-MTrk
-0 Meta 0x21 00
-0 Meta TrkName "http://www.unpronounceable.com/bach"
-0 Meta TrkEnd
-TrkEnd
-MTrk
-0 Meta 0x21 00
-0 Meta TrkName "--------------------------------------"
-0 Meta TrkEnd
-TrkEnd
-MTrk
-0 Meta 0x21 00
-0 Meta TrkName "Original Filename: fp-1all.mid"
-0 Meta TrkEnd
-TrkEnd
-MTrk
-0 Meta 0x21 00
-0 Meta TrkName "Last Modified: March 1, 1997"
-0 Meta TrkEnd
-TrkEnd"""    
-    
-    #notenArray fuellen, notenArray[i] = Note, die zum Zeitpunkt i aktiv ist
-    #falls notenArray[i] == 0, werden die Noten deaktiviert
-    #man beachte: Zeitpunkt wird nur alle zwei i's inkrementiert
-    #Zeitpunkt(i=1) = 0
-    #Zeitpunkt(i=2) = 120
-    #Zeitpunkt(i=3) = 120
-    #Zeitpunkt(i=4) = 240 usw
     print(len(result))
     NotePartMidiString = ""
     lastNoteActive = 0
     for resultLine in range(0, len(result)):
-        
         noteActive = False   
         for resultNotePosition in range(0, result[resultLine].size-1):
             if result[resultLine][resultNotePosition] == 1:
@@ -175,31 +65,10 @@ TrkEnd"""
             NotePartMidiString += "\n"+str(timeStep)+" On ch=1 n="+str(lastNoteActive)+" v=0"
             
     
-    return midiStartString+""+NotePartMidiString+""+Ende
-                
-                
-#   #text schreiben 
-#    for i in range(0, notenArray.size-1):
-#        if notenArray[i] == 0 & i == 0:
-#            megaString = """%s
-#%d On ch=1 n=0 v=0""" % (megaString, timeStep)        
-#        if notenArray[i] == 0 & i != 0:
-#            megaString = """%s
-#%d On ch=1 n=%d v=0""" % (megaString, timeStep, notenArray[i-1])
-#        if notenArray[i] != 0:
-#            megaString = """%s
-#%d On ch=1 n=%d v=100""" % (megaString, timeStep, notenArray[i])
-#            timeStep = timeStep + 120
-#    
-#    megaString = """%s %s""" % (megaString, Ende)
-#    
-#    return megaString
+    return midiStringParts.midiStartString()+""+NotePartMidiString+"\n"+midiStringParts.midiEndeString()
+   
     
-    
-    
-    
-    
-    
+
     
     
     
@@ -224,8 +93,7 @@ print(data.shape)
 model = Sequential()
 model.add(LSTM(88, input_shape=(1,88)))
 model.add(Dense(88, activation='softmax'))
-optimizer = RMSprop(lr=0.01)
-model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+model.compile(loss='categorical_crossentropy', optimizer=RMSprop(lr=0.01))
 
 x_train = numpy.asarray(data)
 y_train = numpy.asarray(target)
@@ -233,9 +101,8 @@ y_train = numpy.asarray(target)
 
 model.fit(x_train, y_train, batch_size=88, epochs=5)
 score = model.evaluate(x_train, y_train, batch_size=88)
-result = model.predict(x_train, batch_size=88)
-#print(score)
-#print(result)
+result = model.predict(x_train[:-10], batch_size=88)
+
 endresult = result
 i = 0
 
@@ -254,12 +121,10 @@ for element in result:
 
 
 
-diggaString = convertToMidi(result)
-print(diggaString)
+finalString = convertToMidi(result)
+print(finalString)
 text_file = open("result55.txt", "w")
-
-text_file.write(diggaString)
-
+text_file.write(finalString)
 text_file.close()
 #numpy.savetxt("result55.txt",diggaString)
 
