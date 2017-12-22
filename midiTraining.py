1db2a891ce87ce96a09ed993b6d1ff882f43b85a
Lars Lemmermann <6lemmerm@informatik.uni-hamburg.de>
2017-12-22 17:46:15 +0100

    toll

--- midiTraining.py
+++ midiTraining.py
@@ -19,41 +19,69 @@ def loadMidiFromText():
         data = myfile.readlines()
         return data
     
-inputArray = midiConverter.getMidiFromFile()
 
-data = inputArray[:-1] # all but last
-target = inputArray[1:] # all but first
+
+
+
+def shapeData(sequence_length):    
+    input_data = []
+    output_data = []
+    
+    for i in range(0, (inputArray.shape[0] - sequence_length), 1):
+        input_sequence = inputArray[i:i+sequence_length]
+        output_sequence = inputArray[i+sequence_length]    
+        input_data.append(input_sequence)
+        output_data.append(output_sequence)
+    
+    input_data = numpy.asarray(input_data)
+    output_data = numpy.asarray(output_data)
+    
+    return (input_data, output_data)
+
+inputArray = midiConverter.getMidiFromFile()
+sequence_length = 10
+
+print("ARRAY"+str(inputArray.shape))
+data,target = shapeData(sequence_length)
+print("DATA"+str(data.shape))
+print("TARGET"+str(target.shape))
+
+#data = inputArray[:-1] # all but last
+#target = inputArray[1:] # all but first
 
 print("inputArray",inputArray.shape)
 
-data = data.reshape((1779, 1, 88))
+#data = data.reshape((1779, 1, 88))
 # target = numpy.array([[ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
 #    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
 #    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
 #    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
 #    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1]])
-data = target.reshape((1779, 1, 88))
+#data = target.reshape((1779, 1, 88))
 
 print("target shape " +str(target.shape))
 print(target)
 print(data.shape)
 model = Sequential()
-model.add(LSTM(88, input_shape=(1,88)))
+model.add(LSTM(88, input_shape=(10,88)))
 model.add(Dense(88, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer=RMSprop(lr=0.01))
 
-x_train = numpy.asarray(data)
+x_train = data
 y_train = numpy.asarray(target)
-testTrain = numpy.zeros((2037, 1, 88))
-for i in range(0, 2036):
-    x = numpy.random.random_integers(0, 87)
-    testTrain[i][0][x] = 1
+print("x_train"+str(x_train.shape))
+print("y_train"+str(y_train.shape))
 
-print("X_TRAIN" + str(x_train))
+#testTrain = numpy.zeros((2037, 1, 88))
+#for i in range(0, 2036):
+#    x = numpy.random.random_integers(0, 87)
+#    testTrain[i][0][x] = 1
+#
+#print("X_TRAIN" + str(x_train))
 
-model.fit(x_train, y_train, batch_size=88, epochs=5)
-score = model.evaluate(x_train, y_train, batch_size=88)
-result = model.predict(testTrain, batch_size=88)
+model.fit(x_train, y_train, batch_size=32, epochs=100)
+score = model.evaluate(x_train, y_train, batch_size=32)
+result = model.predict(x_train, batch_size=32)
 
 endresult = result
 
