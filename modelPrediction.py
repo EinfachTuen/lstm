33bd460ef038767d02a6de53a14dee34f8a60541
Lars <lalemmermann@gmx.de>
2018-01-24 15:32:59 +0100

    Hab angefangen den Code zu kommentieren

--- modelPrediction.py
+++ modelPrediction.py
@@ -1,3 +1,5 @@
+#Diese Datei kümmert sich um die Generierung der neuen Musik.
+#Dazu wird das in midiTraining.py erstellte h5-model benutzt.
 import numpy as numpy
 
 from keras.models import load_model
@@ -5,13 +7,21 @@ from keras.models import load_model
 import json
 import midiConverter
 
-
+#Laden des in midiTraining.py erstellten Models
 model = load_model('my_modelDoubleLstm.h5')
 
 
 actualTrain = numpy.loadtxt("xTrain.txt")
+
+#Initialisierung eines leeren ResultArrays
 result = numpy.empty([1, 88])
-for x in range(0,5000):
+NotesToGenerate = 5000
+
+#Generierung eines ResultArrays 'result' (shape: (NotesToGenerate, 88))
+#das die Wahrscheinlichkeiten der einzelnen Noten zu den jeweilige Zeitschritten enthält
+#result[6][5] enthält also die Wahrscheinlichkeit, dass im sechsten Zeitschritt die
+#Note '5' gespielt wird
+for x in range(0,NotesToGenerate):
     if x > 0:
         actualTrain = actualTrain[1:actualTrain.shape[0]]
         partResult = (model.predict(numpy.array([actualTrain]), batch_size=32))
@@ -24,6 +34,10 @@ for x in range(0,5000):
         print("actualTrain" + str(actualTrain.shape))
         result[0] = partResult[0]
 print("finished saves now")
+
+#"Glättung" der Ergebnisse in 'result'
+#Wahrscheinlichkeiten >0.5 werden zu 1
+#Wahrscheinlichkeiten <0.5 werden zu 0
 i = 0
 for element in result:
     j = 0
@@ -37,6 +51,8 @@ for element in result:
 
 numpy.savetxt("output2.txt",result,fmt='%i')
 
+#Konvertieren des Resultats in einen String,
+#und dann vom String in eine Midi-Datei
 finalString = midiConverter.convertToMidiTrack(result)
 print(finalString)
 text_file = open("result55.txt", "w")
