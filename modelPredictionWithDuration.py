d3b2510ba4953162cdd9ab13697def6851beef38
Hauke <hauke.dau@googlemail.com>
2018-01-24 20:57:33 +0100

    timeDimension added and Files in Folder Reader added.

--- modelPredictionWithDuration.py
+++ modelPredictionWithDuration.py
@@ -5,51 +5,62 @@ from keras.models import load_model
 
 import json
 import midiConverter
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
 
 channelNumber = 25
-model = load_model('./models/'+str(channelNumber)+'_noteModel.h5')
-model_duration = load_model('./models/'+str(channelNumber)+'_durationsModel.h5')
 
-actualTrain = numpy.loadtxt("./log/"+str(channelNumber)+"_x2Train.txt")
-result = numpy.empty([1, 129])
-for x in range(0,500):
-    if x > 0:
-        actualTrain = actualTrain[1:actualTrain.shape[0]]
-        partResult = (model.predict(numpy.array([actualTrain]), batch_size=32))
-        duration = model_duration.predict(numpy.array([actualTrain]), batch_size=32)
-        newresult = numpy.append(partResult[0], duration)
-        partResult = newresult
-        #print("partResult" + str(partResult.shape))
-        actualTrain = numpy.append(actualTrain, [partResult],axis=0)
-        result = numpy.append(result,[partResult],axis=0)
-    else:
-        partResult = (model.predict(numpy.array([actualTrain]), batch_size=32))
-        #print(partResult)
-        duration = model_duration.predict(numpy.array([actualTrain]), batch_size=32)
-        newresult= numpy.append(partResult[0], duration)
-        partResult=newresult
-        #print("partResult" + str(partResult.shape))
-        actualTrain =  numpy.append(actualTrain,[partResult],axis=0)
-        #print("actualTrain" + str(actualTrain.shape))
-        result[0] = partResult[0]
+model = load_model('./models/' + str(channelNumber) + '_noteModel.h5')
+model_duration = load_model('./models/' + str(channelNumber) + '_durationsModel.h5')
 
-print("finsied saves now")
-i = 0
-for element in result:
-    j = 0
-    for prob in element:
-        resultBin = 0
-        if (prob > 0.5):
-            resultBin = 1
-            if(j < 128):
-                result[i][j] = resultBin
-        j = j + 1
-    i = i + 1
+def makePrediction():
+    actualTrain = numpy.loadtxt("./log/"+str(channelNumber)+"_x2Train.txt")
+    result = numpy.empty([1, 129])
+    for x in range(0,500):
+        if x > 0:
+            actualTrain = actualTrain[1:actualTrain.shape[0]]
+            partResult = (model.predict(numpy.array([actualTrain]), batch_size=32))
+            duration = model_duration.predict(numpy.array([actualTrain]), batch_size=32)
+            newresult = numpy.append(partResult[0], duration)
+            partResult = newresult
+            #print("partResult" + str(partResult.shape))
+            actualTrain = numpy.append(actualTrain, [partResult],axis=0)
+            result = numpy.append(result,[partResult],axis=0)
+        else:
+            partResult = (model.predict(numpy.array([actualTrain]), batch_size=32))
+            #print(partResult)
+            duration = model_duration.predict(numpy.array([actualTrain]), batch_size=32)
+            newresult= numpy.append(partResult[0], duration)
+            partResult=newresult
+            #print("partResult" + str(partResult.shape))
+            actualTrain =  numpy.append(actualTrain,[partResult],axis=0)
+            #print("actualTrain" + str(actualTrain.shape))
+            result[0] = partResult[0]
 
-numpy.savetxt("./log/output2.txt",result,fmt='%.3f')
+    print("finsied saves now")
+    i = 0
+    for element in result:
+        j = 0
+        for prob in element:
+            resultBin = 0
+            if (prob > 0.5):
+                resultBin = 1
+                if(j < 128):
+                    result[i][j] = resultBin
+            j = j + 1
+        i = i + 1
 
-requestTests.covertArrayToJSON(result.tolist())
+    numpy.savetxt("./log/output2.txt",result,fmt='%.3f')
+
+    return requestTests.covertArrayToJSON(result.tolist())
 
 
+@app.route('/getPrediction')
+def getPrediction():
+    print("got request")
+    return "<a href='http://localhost/"+makePrediction()+".mid' >"+makePrediction()+"</a>"
 
-
+if __name__ == '__main__':
+    app.run()
--- requestTests.py
+++ requestTests.py
@@ -18,7 +18,7 @@ def GetNotesPlusFloatDuration():
     return input
 
 def covertArrayToJSON(resultVektorArray):
-    r = requests.post("http://localhost/convertArrayToJSON", {"midAsJson": json.dumps(resultVektorArray), "name": "tryOne"})
+    r = requests.post("http://localhost/convertArrayToJSON", {"midAsJson": json.dumps(resultVektorArray), "name": "tryOne2"})
     print(r.status_code, r.reason,r.text)
     return r.text
 
