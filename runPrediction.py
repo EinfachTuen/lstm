e3ae372ab69e569a6df9df69efcdfa73d978463c
haukedau <hauke.dau@googlemail.com>
2018-02-01 23:51:21 +0000

    Large CleanUp, renaming and commentation + documentation

--- runPrediction.py
+++ runPrediction.py
@@ -1,4 +1,9 @@
-import modelPredictionWithDuration
+import prediction
 
-midiFileName = modelPredictionWithDuration.runPrediction("bachOneChannel",73,"bachNow")
+'''
+Type here the Folder and Channel name of the model you like to run the prediction for.
+'''
+channel_name = 73
+folder_Name = "bachOneChannel"
+midiFileName = prediction.runPrediction(folder_Name,channel_name,str(channel_name)+"_"+folder_Name)
 print(midiFileName)
